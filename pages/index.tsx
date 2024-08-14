a33401c7b333e40fa8debab87a246e5ca5a52ffe
Hamza Malik <37263285+hamza7malik@users.noreply.github.com>
2024-08-14 19:49:57 +0400

    first section: logo setup for animation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,24 @@
 import React, { useEffect } from 'react';
 
+import AnimatedLogo from '../components/AnimatedLogo/AnimatedLogo';
+
 const index = () => {
   return (
     <div>
+      {/* section-1 */}
+      <div className='section-content-snap hero-main-wrap h-[100vh] bg-white w-full'>
+        <div className='px-20 my-auto flex items-center justify-center h-full text-center'>
+          <AnimatedLogo />
+        </div>
+      </div>
       <div className='section-snap hero-main-wrap h-[100vh] bg-black w-full'>
         <h1 className='text-white flex items-center justify-center h-full text-center'>
-          Hello World
+          Section-2
+        </h1>
+      </div>
+      <div className='section-snap hero-main-wrap h-[100vh] bg-white w-full'>
+        <h1 className='rotate-element text-black flex items-center justify-center h-full text-center'>
+          Section-3
         </h1>
       </div>
     </div>
