6a42672a55aca89f714a834e40bfbfe9028fb25b
Hamza Malik <37263285+hamza7malik@users.noreply.github.com>
2024-08-14 18:02:53 +0400

    Next, Tailwind, Typescript, Gsap setup, assets extracted

---
+++ next.config.mjs
@@ -0,0 +1,6 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  pagesDirectory: './pages',
+};
+
+export default nextConfig;
---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import React, { useEffect } from 'react';
+
+const index = () => {
+  return (
+    <div>
+      <div className='section-content-snap hero-main-wrap h-[100vh] bg-black w-full'>
+        <h1 className='rotate-element text-white flex items-center justify-center h-full text-center'>
+          Hello World
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default index;
