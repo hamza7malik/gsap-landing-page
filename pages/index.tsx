cd9f5a9ab93c7b828b24b179fcece0b241c8baab
Hamza Malik <37263285+hamza7malik@users.noreply.github.com>
2024-08-14 20:22:43 +0400

    static slides added. 2-6

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,14 +12,29 @@ const index = () => {
         </div>
       </div>
       <div className='section-snap hero-main-wrap h-[100vh] bg-black w-full'>
-        <h1 className='text-white flex items-center justify-center h-full text-center'>
-          Section-2
-        </h1>
+        <h2 className='text-white flex items-center justify-center h-full text-center'>
+          WHAT IS CREO?
+        </h2>
       </div>
       <div className='section-snap hero-main-wrap h-[100vh] bg-white w-full'>
-        <h1 className='rotate-element text-black flex items-center justify-center h-full text-center'>
-          Section-3
-        </h1>
+        <h3 className='rotate-element text-black flex items-center justify-center h-full text-center'>
+          We are not another 360 agency
+        </h3>
+      </div>
+      <div className='section-snap hero-main-wrap h-[100vh] bg-black w-full'>
+        <h3 className='rotate-element text-white flex items-center justify-center h-full text-center'>
+          But rather a mindset
+        </h3>
+      </div>
+      <div className='section-snap hero-main-wrap h-[100vh] bg-white w-full'>
+        <h3 className='rotate-element text-black flex items-center justify-center h-full text-center'>
+          that will challenge you
+        </h3>
+      </div>
+      <div className='section-snap hero-main-wrap h-[100vh] bg-white w-full'>
+        <h3 className='rotate-element text-black flex items-center justify-center h-full text-center'>
+          To rethink
+        </h3>
       </div>
     </div>
   );
