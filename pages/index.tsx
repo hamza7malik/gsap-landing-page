7d773ed4549529bb7a0626f6b168092dd4cda778
Hamza Malik <37263285+hamza7malik@users.noreply.github.com>
2024-08-14 18:40:41 +0400

    webpack and svg setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,8 @@ import React, { useEffect } from 'react';
 const index = () => {
   return (
     <div>
-      <div className='section-content-snap hero-main-wrap h-[100vh] bg-black w-full'>
-        <h1 className='rotate-element text-white flex items-center justify-center h-full text-center'>
+      <div className='section-snap hero-main-wrap h-[100vh] bg-black w-full'>
+        <h1 className='text-white flex items-center justify-center h-full text-center'>
           Hello World
         </h1>
       </div>
